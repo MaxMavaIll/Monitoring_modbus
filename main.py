9682f49ac9c28f489703729edc5217a27a640fa1
Black1Matter <blackmatter19.70@gmail.com>
2024-11-13 19:17:15 +0200

    add function for transfer time utc -> kyiv

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import os
 import toml
-import datetime
 import time
+import pytz
 
+from datetime import datetime
 from modbus_dl.scripts import  modbus_helper
-from ORM_MySql.connect_mysql import connect_DB, add_register_record, disconnect, get_daily_averages
+from ORM_MySql.mysql import connect_DB, add_register_record, disconnect, get_daily_averages
 from API_sheet.sheets_update import update_sheets
 
 config_toml = toml.load('config.toml')
 
+def get_time_kyiv_with_utc(timestamp, time_format = '%Y-%m-%d %H:%M:%S%z') -> datetime:
+    utc_zone = pytz.timezone('UTC')
+    kyiv_zone = pytz.timezone('Europe/Kiev')
 
-def main(time_format = '%Y-%m-%d %H:%M:%S%z'):
+    if timestamp is None:
+        timestamp_kyiv = datetime.now(kyiv_zone)
+        return timestamp_kyiv
+
+    timestamp_utc = datetime.strptime(timestamp, time_format)
+    timestamp_kyiv = timestamp_utc.astimezone(kyiv_zone)
+    # timestamp_kyiv = timestamp_kyiv.strftime(time_format)
+
+    print("Original UTC time:", timestamp_utc)
+    print("Converted Kiev time:", timestamp_kyiv)
+
+    return timestamp_kyiv
+
+
+
+
+def main():
+
     averages = {}
-    current_time = datetime.datetime.now()
+    current_time = datetime.now()
 
     config_path = os.path.realpath(__file__).replace('main.py', 'config.toml')
     modbus_logger = modbus_helper.ModbusTCPDataLogger(
@@ -27,14 +48,16 @@ def main(time_format = '%Y-%m-%d %H:%M:%S%z'):
         )
 
     for town, records in modbus_logger.data_for_db.items():
+        time_utc = records.get('timestamp_utc')
+        kyiv_time = get_time_kyiv_with_utc(time_utc)
+
+        
         if records is None:
             add_register_record(
-                town=town, session=session, engine=engine)
+                town=town, session=session, engine=engine, timestamp=kyiv_time)
             continue
 
         compound = records.get('compound')
-        timestamp = datetime.datetime.strptime(records.get('timestamp_utc'), time_format)
-
         if not compound:
             compound = {}
 
@@ -43,7 +66,7 @@ def main(time_format = '%Y-%m-%d %H:%M:%S%z'):
             CO=compound.get('CO'), SO2=compound.get('SO2'), NO2=compound.get('NO2'), 
             NO=compound.get('NO'), H2S=compound.get('H2S'), O3=compound.get('O3'), 
             NH3=compound.get('NH3'), PM2_5=compound.get('PM2_5'), PM10=compound.get('PM10'), 
-            timestamp = timestamp, session=session, engine=engine
+            timestamp = kyiv_time, session=session, engine=engine
             )
         
         if current_time.hour == 0 and current_time.minute == 0:
